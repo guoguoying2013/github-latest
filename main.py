e1d037d3c69445262c96ff5869a3998783b01516
Ying Guo <guoguoying2013@gmail.com>
2019-05-12 14:27:44 -0700

    submit

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import sys
 import json
-
 import requests
+from datetime import datetime
 
 # Use Like python githubber.py JASchilz
-# (or another user name)
+
+
+def list_of_events(user_name):
+    """
+    it returned a list of events that this user_name has done.
+    """
+    address='https://api.github.com/users/{}/events'.format(user_name)
+    response = requests.get(address)
+    return response.json()
+
+def time_stampe_of_the_first_event(user_name):
+    """
+    Print out the time stamp associated with the first event in event list
+    """
+    lst_events = list_of_events(user_name)
+    first_event = lst_events[0]
+    time_string = first_event['created_at']
+    datetime_object = datetime.strptime(time_string, "%Y-%m-%dT%H:%M:%SZ")
+    timestamp = datetime.timestamp(datetime_object)
+    return timestamp
 
 if __name__ == "__main__":
     username = sys.argv[1]
-
-    # TODO:
-    #
     # 1. Retrieve a list of "events" associated with the given user name
+    print(f"this is the list of events done by user {username}")
+    lst = list_of_events(username)
+    for i in lst:
+        print(i['type'])
     # 2. Print out the time stamp associated with the first event in that list.
+    print("the time stamp associated with the first event in that list is below:")
+    print(time_stampe_of_the_first_event(username))
 
-    print("COMPLETE THE TODOs")
-    
 
 
